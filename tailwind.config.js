9c67addeb96ebc9bda55af991ba348111b7b72e2
syedsimanta03 <syed.simanta10@gmail.com>
2024-01-14 18:56:10 +0600

    changed

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './assets/*.liquid',
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/**/*.liquid'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
